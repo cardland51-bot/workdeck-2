5287aa569d545216cdff59e0ef6384d7b1596fe6
VanCole <cardland51@gmail.com>
2025-11-11 15:01:38 -0600

    Create health.js

---
+++ functions/api/health.js
@@ -0,0 +1,7 @@
+export const onRequestGet = async ({ env }) => {
+  const ok = !!env.R2
+  const hasKey = !!env.OPENAI_API_KEY
+  return new Response(JSON.stringify({ ok, hasKey, time: new Date().toISOString() }), {
+    headers: { 'content-type': 'application/json' }
+  })
+}
